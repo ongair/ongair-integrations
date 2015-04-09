bcbc254e6a3adb8ef81113783742a6e3455d88b1
Trevor Kimenye <kimenye@gmail.com>
2015-04-09 11:28:45 +0300

    Cleaned up zendesk url creation in stubs

--- spec/ongair_spec.rb
+++ spec/ongair_spec.rb
@@ -18,49 +18,41 @@ describe 'The Ongair Integrations API' do
       token = '1234567890'
       zendesk_url = 'test.zendesk.com/api/v2'
       ongair_phone_number = '254123456789'
-      ongair_url = "http://41.242.1.46/api/notifications?ticket={{ticket.id}}&account=#{ongair_phone_number}"
-      target_url = "http://41.242.1.46/api/tickets/status_change?ticket={{ticket.id}}&account=#{ongair_phone_number}&status={{ticket.status}}"
 
-# with(:body => "{\"target\":{\"type\":\"url_target\",\"title\":\"Ongair - Ticket commented on\",\"target_url\":\"http://41.242.1.46/api/notifications?ticket={{ticket.id}}&account=254123456789\",\"attribute\":\"comment\",\"method\":\"POST\"}}").
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/targets").         
-         # with(:body => { target: { type: 'url_target', title: 'Ongair - Ticket commented on', target_url: ongair_url, attribute: 'comment', method: 'POST' } }.to_json.to_s ).
-         with(:body => "{\"target\":{\"type\":\"url_target\",\"title\":\"Ongair - Ticket commented on\",\"target_url\":\"#{ongair_url}\",\"attribute\":\"comment\",\"method\":\"POST\"}}").
+      ongair_url = "#{Ongair.config.app_url}/api/notifications?ticket={{ticket.id}}&account=#{ongair_phone_number}"
+      target_url = "#{Ongair.config.app_url}/api/tickets/status_change?ticket={{ticket.id}}&account=#{ongair_phone_number}&status={{ticket.status}}"
+
+      base_zendesk_url = "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}"
+
+      stub_request(:post, "#{base_zendesk_url}/targets").         
+         with(:body => { target: { type: 'url_target', title: 'Ongair - Ticket commented on', target_url: ongair_url, attribute: 'comment', method: 'POST' } }.to_json.to_s ).         
          to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/triggers").
-       with(:body => "{\"trigger\":{\"title\":\"Ongair - Ticket commented on\",\"conditions\":{\"all\":[{\"field\":\"update_type\",\"operator\":\"is\",\"value\":\"Change\"},{\"field\":\"comment_is_public\",\"operator\":\"is\",\"value\":\"requester_can_see_comment\"},{\"field\":\"comment_is_public\",\"operator\":\"is\",\"value\":\"true\"}]},\"actions\":[{\"field\":\"notification_target\",\"value\":[null,\"{{ticket.latest_comment}}\"]}],\"output\":null}}").
+      stub_request(:post, "#{base_zendesk_url}/triggers").        
+       with(:body => "{\"trigger\":{\"title\":\"Ongair - Ticket commented on\",\"conditions\":{\"all\":[{\"field\":\"update_type\",\"operator\":\"is\",\"value\":\"Change\"},{\"field\":\"comment_is_public\",\"operator\":\"is\",\"value\":\"requester_can_see_comment\"},{\"field\":\"comment_is_public\",\"operator\":\"is\",\"value\":\"true\"}]},\"actions\":[{\"field\":\"notification_target\",\"value\":[null,\"{{ticket.latest_comment}}\"]}],\"output\":null}}").       
        to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/triggers").
+      stub_request(:post, "#{base_zendesk_url}/triggers").
         with(:body => "{\"trigger\":{\"title\":\"Ongair - Notify requester of received request via WhatsApp\",\"conditions\":{\"all\":[{\"field\":\"update_type\",\"operator\":\"is\",\"value\":\"Create\"},{\"field\":\"status\",\"operator\":\"is_not\",\"value\":\"solved\"}],\"any\":[]},\"actions\":[{\"field\":\"notification_target\",\"value\":[null,\"Your request has been received and is being reviewed by our support staff.\\n\\nTo add additional comments, reply to this message.\"]}],\"output\":null}}").
         to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/targets").
+      stub_request(:post, "#{base_zendesk_url}/targets").
          with(:body => "{\"target\":{\"type\":\"url_target\",\"title\":\"Ongair - Ticket status changed\",\"target_url\":\"#{target_url}\",\"attribute\":\"comment\",\"method\":\"POST\"}}").
          to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/triggers").
+      stub_request(:post, "#{base_zendesk_url}/triggers").
          with(:body => "{\"trigger\":{\"title\":\"Ongair - Ticket status changed\",\"conditions\":{\"all\":[{\"field\":\"status\",\"operator\":\"changed\",\"value\":null}],\"any\":[]},\"actions\":[{\"field\":\"notification_target\",\"value\":[null,\"The status of your ticket has been changed to {{ticket.status}}\"]}],\"output\":null}}").
          to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://test%40domain.com%2Ftoken:1234567890@test.zendesk.com/api/v2/targets").
-         with(:body => "{\"target\":{\"type\":\"url_target\",\"title\":\"Ongair - Ticket commented on\",\"target_url\":\"http://41.242.1.46/api/notifications?ticket={{ticket.id}}&account=254123456789&comment={{ticket.latest_comment}}\",\"attribute\":\"comment\",\"method\":\"POST\"}}",
-              :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
+      stub_request(:post, "#{base_zendesk_url}/targets").
+         with(:body => "{\"target\":{\"type\":\"url_target\",\"title\":\"Ongair - Ticket commented on\",\"target_url\":\"http://41.242.1.46/api/notifications?ticket={{ticket.id}}&account=254123456789&comment={{ticket.latest_comment}}\",\"attribute\":\"comment\",\"method\":\"POST\"}}").
          to_return(:status => 200, :body => "", :headers => {})
 
       post '/api/accounts', { ongair_phone_number: '254123456789',  zendesk_url: "https://#{zendesk_url}", zendesk_access_token: '1234567890', ongair_token: '087654321', ongair_url: 'http://app.ongair.im', zendesk_user: email }
-      puts response.body
       expect_json({ success: true })
     end
   end
 
-  describe 'How to get a ticket from ZenDesk' do
-    it 'Needs a real ticket' do
-      get '/api/tickets/', { id: 'doesnotexist', account: '2541234567890' }
-
-
-    end
-  end
 
   describe 'Ticket creation' do
     it 'creates ZenDesk ticket' do
@@ -69,35 +61,36 @@ describe 'The Ongair Integrations API' do
       token = '1234567890'
       zendesk_url = 'test.zendesk.com/api/v2'
       ongair_phone_number = '254123456789'
+      base_zendesk_url = "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}"
 
-      stub_request(:get, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/tickets").
+      stub_request(:get, "#{base_zendesk_url}/tickets").
                with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:get, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/users").
+      stub_request(:get, "#{base_zendesk_url}/users").
                with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/users").
+      stub_request(:post, "#{base_zendesk_url}/users").
                with(:body => "{\"user\":{\"name\":\"jsk\",\"phone\":\"254722881199\"}}",
                     :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:get, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/ticket_fields").
+      stub_request(:get, "#{base_zendesk_url}/ticket_fields").
                with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/ticket_fields").
+      stub_request(:post, "#{base_zendesk_url}/ticket_fields").
                with(:body => "{\"ticket_field\":{\"type\":\"text\",\"title\":\"Phone number\"}}",
                     :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/tickets").
+      stub_request(:post, "#{base_zendesk_url}/tickets").
                with(:body => "{\"ticket\":{\"subject\":\"254722881199#1\",\"comment\":{\"value\":\"Hi\"},\"submitter_id\":null,\"requester_id\":null,\"priority\":\"Urgent\",\"custom_fields\":[{\"id\":null,\"value\":\"254722881199\"}]}}",
                     :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
 
-      stub_request(:post, "https://#{CGI.escape(email + '/')}token:#{token}@#{zendesk_url}/tickets").
+      stub_request(:post, "#{base_zendesk_url}/tickets").
                with(:body => "{\"ticket\":{\"subject\":\"254722881199#1\",\"comment\":{\"value\":\"Image attached\"},\"submitter_id\":null,\"requester_id\":null,\"priority\":\"Urgent\",\"custom_fields\":[{\"id\":null,\"value\":\"254722881199\"}]}}",
                     :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI API 1.5.1'}).
                to_return(:status => 200, :body => "", :headers => {})
