77a25717cb18ffc36885486bdd6cbdf6b72aa2eb
muaad <muadh24@gmail.com>
2015-04-29 14:40:28 +0300

    Added method to send image through WhatsApp

--- whatsapp.rb
+++ whatsapp.rb
@@ -3,6 +3,10 @@ class WhatsApp
 	  HTTParty.post("#{account.ongair_url}/api/v1/base/send?token=#{account.ongair_token}", body: {phone_number: phone_number, text: message, thread: true})
 	end
 
+	def self.send_image account, phone_number, image_url
+		HTTParty.post("#{account.ongair_url}/api/v1/base/send_image", body: { token: account.ongair_token,  phone_number: phone_number, image: image_url, thread: true })
+	end
+
 	def self.send_location latitude, longitude, phone_number
 	  location = Location.find_nearest latitude, longitude
 	  HTTParty.post("#{location.account.ongair_url}/api/v1/base/send?token=#{location.account.ongair_token}", body: {phone_number: phone_number, text: location.address, thread: true})
