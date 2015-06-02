537b2523a557580bfdf79098117757ae7a2ffab6
muaad <muadh24@gmail.com>
2015-06-02 16:38:02 +0300

    Added some Rollbar options

--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ require 'rollbar'
 
 Rollbar.configure do |config|
   config.access_token = Ongair.config.rollbar_access_token
+  config.enabled = Ongair.config.rollbar_enabled
 end
 
 use Rack::Reloader, 0
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,6 @@ Ongair.configure do |config|
   config.root     = File.dirname(__FILE__)
   yml = YAML.load(File.read("config/application.yml"))
   config.app_url = yml['app_url']
+  config.rollbar_access_token = yml['rollbar_access_token']
+  config.rollbar_enabled = yml['rollbar_enabled']
 end
