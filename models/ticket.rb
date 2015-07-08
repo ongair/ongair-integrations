555a8ffcfe6612a7d53bf541e530745ee3169e10
muaad <muadh24@gmail.com>
2015-07-08 12:11:42 +0300

    Find unsolved tickets by the digits that represent the actual ticket statuses

--- models/ticket.rb
+++ models/ticket.rb
@@ -5,7 +5,8 @@ class Ticket < ActiveRecord::Base
 	belongs_to :user
 
 	scope :zendesk, -> { where("source = ?", "Zendesk") }
-	scope :unsolved, -> {  where("status = ? or status = ? or status = ?", "open", "pending", "new") }
+	# scope :unsolved, -> {  where("status = ? or status = ? or status = ?", "open", "pending", "new") }
+	scope :unsolved, -> {  where("status = ? or status = ? or status = ?", "1", "2", "3") }
 
 	def self.unsolved_zendesk_tickets account, phone_number
 		Ticket.zendesk.unsolved.where("account_id = ? and phone_number = ?", account.id, phone_number)
