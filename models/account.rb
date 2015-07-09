46268d64b0b94838a3a80eff2968be8df53ddd46
muaad <muadh24@gmail.com>
2015-07-09 12:44:37 +0300

    Added method to create users for account based on the tickets

--- models/account.rb
+++ models/account.rb
@@ -1,4 +1,5 @@
 require "active_record"
 class Account < ActiveRecord::Base
 	has_many :tickets
+	has_many :users
 end
--- models/user.rb
+++ models/user.rb
@@ -2,4 +2,15 @@ require "active_record"
 
 class User < ActiveRecord::Base
 	has_many :tickets
+	belongs_to :account
+
+	def self.create_user_for_each_account
+		Ticket.all.each do |ticket|
+			account = ticket.account
+			zt = Zendesk.find_ticket(account, ticket.ticket_id)
+			user = User.find_or_create_by! phone_number: ticket.phone_number, account: account
+			user.update zendesk_id: zt.comments.first.author_id if !zt.nil?
+			ticket.update user: user
+		end
+	end
 end
