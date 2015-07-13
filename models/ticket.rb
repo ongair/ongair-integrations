d24fce253bad9add51c33785f99282c3d99806a9
muaad <muadh24@gmail.com>
2015-07-13 11:46:11 +0300

    Added Dutch word for solved: 'opgelost'

--- models/ticket.rb
+++ models/ticket.rb
@@ -26,7 +26,7 @@ class Ticket < ActiveRecord::Base
 		status_new = {STATUS_NEW => ["new", "nuevo", "novo", "nieuw"]}
 		status_open = {STATUS_OPEN => ["open", "abierto", "aberto", "offen"]}
 		status_pending = {STATUS_PENDING => ["pending", "pendiente", "in afwachting"]}
-		status_solved = {STATUS_SOLVED => ["solved", "resuelto", "resolvido"]}
+		status_solved = {STATUS_SOLVED => ["solved", "resuelto", "resolvido", "opgelost"]}
 		status_closed = {STATUS_CLOSED => ["closed", "cerrado"]}
 		status_dictionary = {status_new: status_new, status_open: status_open, status_pending: status_pending, status_solved: status_solved, status_closed: status_closed}
 	end
