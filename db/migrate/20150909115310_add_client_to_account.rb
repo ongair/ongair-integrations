9be49e80233cbd3d8685989d3ab5044a6d92e919
muaad <muadh24@gmail.com>
2015-09-09 15:32:12 +0300

    Fix some typo in migration

--- db/migrate/20150909115310_add_client_to_account.rb
+++ db/migrate/20150909115310_add_client_to_account.rb
@@ -1,6 +1,6 @@
 class AddClientToAccount < ActiveRecord::Migration
   def change
     add_reference :accounts, :client, index: true
-    add_foreign_key :accounts, :users
+    add_foreign_key :accounts, :client
   end	
 end
