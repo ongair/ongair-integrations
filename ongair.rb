9deb28110facce24668987a1f4e45c771725de08
muaad <muadh24@gmail.com>
2015-09-14 12:33:39 +0300

    Check if Zendesk ticket is not nil

--- ongair.rb
+++ ongair.rb
@@ -208,27 +208,30 @@ module Ongair
       
       post do
         if !account.nil?
-          comment = Zendesk.find_ticket(account, params[:ticket].to_i).comments.last
-          ticket = Ticket.find_by(ticket_id: params[:ticket].to_i, account: account)
-          if !ticket.nil?
-            phone_number = ticket.phone_number # Zendesk.find_phone_number_for_ticket(account, params[:ticket].to_i)
+          zen_ticket = Zendesk.find_ticket(account, params[:ticket].to_i)
+          if !zen_ticket.nil?
+            comment = Zendesk.find_ticket(account, params[:ticket].to_i).comments.last
+            ticket = Ticket.find_by(ticket_id: params[:ticket].to_i, account: account)
+            if !ticket.nil?
+              phone_number = ticket.phone_number # Zendesk.find_phone_number_for_ticket(account, params[:ticket].to_i)
 
-            if params.has_key?(:author)
-              user = User.where(zendesk_id: params[:author], account: account).first
-            else
-              user = User.where(zendesk_id: comment.author_id, account: account).first
-            end
-            
-            if (!ticket.nil? && ticket.user != user) || user.nil?
-              # Send ticket comment through WhatsApp
-              WhatsApp.send_message account, phone_number, params[:comment]
+              if params.has_key?(:author)
+                user = User.where(zendesk_id: params[:author], account: account).first
+              else
+                user = User.where(zendesk_id: comment.author_id, account: account).first
+              end
+              
+              if (!ticket.nil? && ticket.user != user) || user.nil?
+                # Send ticket comment through WhatsApp
+                WhatsApp.send_message account, phone_number, params[:comment]
 
-              attachments = comment.attachments
-              if !attachments.empty?
-                files = attachments.collect{|a| a.content_url if (a.content_type && a.content_type.split("/")[0] == "image") }.compact
-                # Send image through WhatsApp
-                files.each do |image_url|
-                  WhatsApp.send_image account, phone_number, image_url
+                attachments = comment.attachments
+                if !attachments.empty?
+                  files = attachments.collect{|a| a.content_url if (a.content_type && a.content_type.split("/")[0] == "image") }.compact
+                  # Send image through WhatsApp
+                  files.each do |image_url|
+                    WhatsApp.send_image account, phone_number, image_url
+                  end
                 end
               end
             end
