d75b3f67c55003d3b989803d28f96e5384976584
Muaad <muadh24@gmail.com>
2015-11-02 18:01:50 +0300

    Added a way to avoid creating multiple users for the same number on Zendesk, update name for users named 'Infobip User'

--- zendesk.rb
+++ zendesk.rb
@@ -73,17 +73,25 @@ class Zendesk
     field
   end
 
+  def self.find_user account, id
+    client = self.client(account)
+    client.users.find(id: id)
+  end
+
   def self.find_user_by_phone_number account, phone_number
     client = self.client(account)
     client.users.search(query: phone_number).first
   end
 
-  def self.find_or_create_user account, name, phone_number
+  def self.create_user account, name, phone_number
     client = self.client(account)
-    if self.find_user_by_phone_number(account, phone_number).nil?
-      user = ZendeskAPI::User.create(client, { name: name, phone: phone_number })
-    else
-      user = self.find_user_by_phone_number(account, phone_number)
+    ZendeskAPI::User.create(client, { name: name, phone: phone_number })
+  end
+
+  def self.find_or_create_user account, name, phone_number
+    user = self.find_user_by_phone_number(account, phone_number)
+    if user.nil?
+      user = Zendesk.create_user(account, name, phone_number)
     end
     user
   end
@@ -169,8 +177,10 @@ class Zendesk
     end
   end
 
-  def self.setup_ticket notification_type, phone_number, zen_user_id, account, user, text, image, tickets
+  def self.setup_ticket notification_type, account, user, text, image, tickets
     ticket = nil
+    phone_number = user.phone_number
+    zen_user_id = user.zendesk_id
     tags = ['Ongair', phone_number]
     tags.push(account.name) if !account.name.blank?
     if notification_type == "MessageReceived"
@@ -195,16 +205,25 @@ class Zendesk
   def self.create_ticket phone_number, name, text, notification_type, image="", account
     ticket = nil
     tickets = Ticket.unsolved_zendesk_tickets account, phone_number
-    zen_user = Zendesk.find_or_create_user(account, name, phone_number)
 
     user = User.find_or_create_by!(phone_number: phone_number, account: account)
-    if !zen_user.nil?
-      zen_user_id = zen_user.id
-      user.update zendesk_id: zen_user_id
+    if user.zendesk_id.nil?
+      zen_user = Zendesk.find_or_create_user(account, name, phone_number)
+      user.update zendesk_id: zen_user.id
+    else
+      zen_user = Zendesk.find_user(account, user.zendesk_id)
+      if zen_user.name == "Infobip User"
+        zen_user.name = name
+        zen_user.save!
+      end
+      if zen_user.nil?
+        zen_user = Zendesk.create_user(account, name, phone_number)
+        user.update zendesk_id: zen_user.id
+      end
     end
 
     if tickets.size == 0
-      ticket = self.setup_ticket notification_type, phone_number, zen_user_id, account, user, text, image, tickets
+      ticket = self.setup_ticket notification_type, account, user, text, image, tickets
       if !ticket.nil? && !account.response.blank?
         WhatsApp.send_message(account, phone_number, WhatsApp.personalize_message(account.response, ticket.id, name))
       end
@@ -215,13 +234,13 @@ class Zendesk
       if !ticket.nil?
         if notification_type == "MessageReceived"
           current_ticket.update(user_id: user.id) if current_ticket.user.nil?
-          ticket.comment = { :value => text, :author_id => zen_user_id }
+          ticket.comment = { :value => text, :author_id => user.zendesk_id }
           if Ticket.get_status(ticket.status) == "3" || Ticket.get_status(ticket.status) == "4"
             ticket.status = "open"
             current_ticket.update(status: "2")
           end
         elsif notification_type == "ImageReceived"
-          ticket.comment = { :value => "Image attached", :author_id => zen_user_id }
+          ticket.comment = { :value => "Image attached", :author_id => user.zendesk_id }
           if Ticket.get_status(ticket.status) == "3" || Ticket.get_status(ticket.status) == "4"
             ticket.status = "open"
             current_ticket.update(status: "2")
@@ -233,7 +252,7 @@ class Zendesk
         begin
           ticket.save!
         rescue ZendeskAPI::Error::RecordInvalid => e
-          ticket = self.setup_ticket notification_type, phone_number, zen_user_id, account, user, text, image, tickets
+          ticket = self.setup_ticket notification_type, account, user, text, image, tickets
           if !ticket.nil? && !account.response.blank?
             WhatsApp.send_message(account, phone_number, WhatsApp.personalize_message(account.response, ticket.id, name))
           end
@@ -241,7 +260,7 @@ class Zendesk
         `rm image.png` if notification_type == "ImageReceived"
       else
         orphan = tickets.last
-        ticket = self.setup_ticket notification_type, phone_number, zen_user_id, account, user, text, image, tickets
+        ticket = self.setup_ticket notification_type, account, user, text, image, tickets
 
         if !ticket.nil? && !account.response.blank?
           WhatsApp.send_message(account, phone_number, WhatsApp.personalize_message(account.response, ticket.id, name))
